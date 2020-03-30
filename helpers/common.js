8a413f02fe6ecf31de013227b4d65b599dd72ec7
annydev <dandara.anna13@hotmail.com>
2020-03-30 22:40:06 +0300

    authorize module added

--- helpers/common.js
+++ helpers/common.js
@@ -1,7 +1,17 @@
-module.exports = {
-    Authorize: (req, res) => {
-        // if (!req.isAuthenticated()) {
+const CommonModule = (function() {
+    // Preperties
+
+    const self = this;
+
+    // Public functions
+  
+    self.Authorize = function() {
+       // if (!req.isAuthenticated()) {
         //     res.redirect("/login");
         // }
-    }
-}
+    };
+    
+    return self;
+})();
+
+module.exports = { ...CommonModule }
