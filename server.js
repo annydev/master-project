57bc09966121403b2af0b3d7c657dff6fc4e6c52
annydev <dandara.anna13@hotmail.com>
2020-05-31 15:03:36 +0300

    deploy changes

--- server.js
+++ server.js
@@ -29,6 +29,6 @@ mongoose.set("useCreateIndex", true);
 
 routing.Init(app);
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server has started sucessefuly");
 });
